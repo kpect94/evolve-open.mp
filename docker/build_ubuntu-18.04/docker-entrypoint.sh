b8be6299b76d45943be3adeeabea155541880387
zow1e2k <kadomodify@gmail.com>
2025-09-07 19:02:11 +0400

    fix: build gcc-8 docker

--- docker/build_ubuntu-18.04/docker-entrypoint.sh
+++ docker/build_ubuntu-18.04/docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 [ -z $CONFIG ] && config=Release || config="$CONFIG"
-[ -z $BUILD_SHARED ] && build_shared=1 || build_shared="$BUILD_SHARED"
+[ -z $BUILD_SHARED ] && build_shared=0 || build_shared="$BUILD_SHARED"
 [ -z $BUILD_SERVER ] && build_server=1 || build_server="$BUILD_SERVER"
 [ -z $BUILD_TOOLS ] && build_tools=0 || build_tools="$BUILD_TOOLS"
 [ -z $TARGET_BUILD_ARCH ] && target_build_arch=x86 || target_build_arch="$TARGET_BUILD_ARCH"
